358c8d791e3d290927aebcb31c86fa3dfeb41115
Vetri L <vsvetri9@gmail.com>
2026-03-10 01:02:16 -0700

    practiced destructuring

---
+++ structuring & destructuring/ex2.js
@@ -0,0 +1,20 @@
+// skipping values in arrays
+const arr=["vetri","virat","rohit","root","abd"];
+const [a,,c,,e]=arr;
+console.log(a);
+console.log(c);
+console.log(e);
+
+
+// DEFAULT VALUES:
+/*const arr1=[10,20];
+const[one,two,three=30]=arr1;
+console.log(one)
+console.log(two)
+console.log(three)*/
+
+const arr1=["vetri","virat"];
+const[one,two,three="patidhar"]=arr1;
+console.log(one)
+console.log(two)
+console.log(three)
